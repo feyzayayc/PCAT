8ca5a0ba841396092708713e4ea594e61d3897b4
feyzayayci <rfeyzay@gmail.com>
2022-05-25 23:38:35 +0300

    commit

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+
 const app = express();
+// Template Engine
+app.set("view engine", "ejs");
 
 // middleware
 app.use(express.static('public'));
 
-const port = 3000;
+// routers
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
-})
+    res.render("./index");
+});
+app.get('/about', (req, res) => {
+    res.render("./about");
+});
+app.get('/add', (req, res) => {
+    res.render("./add");
+});
+
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`listening on port:${port}`);
